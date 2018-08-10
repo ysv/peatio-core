3d845fa5d99b13d534a42f4533080bcf73fc85b8
Stanislav Seletskiy <s.seletskiy@gmail.com>
2018-08-10 16:01:32 +0300

    upstream: print orderbook

--- lib/peatio/command/service.rb
+++ lib/peatio/command/service.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module Peatio::Command::Service
-
   class Start < Peatio::Command::Base
     class Ranger < Peatio::Command::Base
       def execute
@@ -11,9 +10,27 @@ module Peatio::Command::Service
 
     class UpstreamBinance < Peatio::Command::Base
       def execute
-        ::Peatio::Upstream::Binance.run!(
-          markets: ["ethbtc"]
-        )
+        EM.run {
+          orderbooks = ::Peatio::Upstream::Binance.run!(
+            markets: ["ethbtc"],
+          )
+
+          logger = Peatio::Upstream::Binance.logger
+
+          EM::PeriodicTimer.new(5) do
+            orderbooks.each do |symbol, orderbook|
+              asks, bids = orderbook.depth(5)
+
+              asks.each do |(price, volume)|
+                logger.info "[#{symbol}] ASK #{price} #{volume}"
+              end
+
+              bids.each do |(price, volume)|
+                logger.info "[#{symbol}] BID #{price} #{volume}"
+              end
+            end
+          end
+        }
       end
     end
 
@@ -24,5 +41,4 @@ module Peatio::Command::Service
   class Root < Peatio::Command::Base
     subcommand "start", "Start a service", Start
   end
-
 end
